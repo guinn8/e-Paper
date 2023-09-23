0d07a41fc5336971a6af56a430f5221bc5eabf5c
Gavin Guinn <gavinguinn1@gmail.com>
2023-09-23 04:17:05 +0100

    localizing files

--- examples/main.c
+++ examples/main.c
@@ -17,6 +17,44 @@ void  Handler(int signo)
     exit(0);
 }
 
+// Create and configure image buffer
+UBYTE* createAndConfigImage() {
+    UWORD Imagesize = ((EPD_2in13_V3_WIDTH % 8 == 0) ? (EPD_2in13_V3_WIDTH / 8) : (EPD_2in13_V3_WIDTH / 8 + 1)) * EPD_2in13_V3_HEIGHT;
+    UBYTE *BlackImage = (UBYTE *)malloc(Imagesize);
+    if(BlackImage == NULL) {
+        Debug("Failed to apply for black memory...\r\n");
+        exit(-1);
+    }
+    Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 90, WHITE);
+    Paint_Clear(WHITE);
+    return BlackImage;
+}
+
+void finalizeEPD(UBYTE *BlackImage) {
+    EPD_2in13_V3_Sleep();
+    free(BlackImage);
+    DEV_Delay_ms(2000);
+    DEV_Module_Exit();
+}
+
+// Initialize E-Paper Display (EPD)
+void initEPD() {
+    Debug("EPD_2in13_V3_test Demo\r\n");
+    if(DEV_Module_Init() != 0) {
+        exit(-1);
+    }
+    EPD_2in13_V3_Init();
+}
+
+// Measure the time required to clear the EPD
+void measureClearTime() {
+    struct timespec start = {0,0}, finish = {0,0};
+    clock_gettime(CLOCK_REALTIME, &start);
+    EPD_2in13_V3_Clear();
+    clock_gettime(CLOCK_REALTIME, &finish);
+    Debug("%ld S\r\n", finish.tv_sec - start.tv_sec);
+}
+
 
 int main(void)
 {
--- examples/number_box.c
+++ examples/number_box.c
@@ -9,44 +9,12 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-// Initialize E-Paper Display (EPD)
-void initEPD() {
-    Debug("EPD_2in13_V3_test Demo\r\n");
-    if(DEV_Module_Init() != 0) {
-        exit(-1);
-    }
-    EPD_2in13_V3_Init();
-}
 
-// Measure the time required to clear the EPD
-void measureClearTime() {
-    struct timespec start = {0,0}, finish = {0,0};
-    clock_gettime(CLOCK_REALTIME, &start);
-    EPD_2in13_V3_Clear();
-    clock_gettime(CLOCK_REALTIME, &finish);
-    Debug("%ld S\r\n", finish.tv_sec - start.tv_sec);
-}
 
-// Create and configure image buffer
-UBYTE* createAndConfigImage() {
-    UWORD Imagesize = ((EPD_2in13_V3_WIDTH % 8 == 0) ? (EPD_2in13_V3_WIDTH / 8) : (EPD_2in13_V3_WIDTH / 8 + 1)) * EPD_2in13_V3_HEIGHT;
-    UBYTE *BlackImage = (UBYTE *)malloc(Imagesize);
-    if(BlackImage == NULL) {
-        Debug("Failed to apply for black memory...\r\n");
-        exit(-1);
-    }
-    Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 90, WHITE);
-    Paint_Clear(WHITE);
-    return BlackImage;
-}
+
 
 // Finalize EPD and free resources
-void finalizeEPD(UBYTE *BlackImage) {
-    EPD_2in13_V3_Sleep();
-    free(BlackImage);
-    DEV_Delay_ms(2000);
-    DEV_Module_Exit();
-}
+
 
 #define TOTAL_BOXES 7
 #define EPD_WIDTH EPD_2in13_V3_HEIGHT
@@ -57,8 +25,6 @@ void drawTitle(const char *title) {
     Paint_DrawRectangle(0, 0, EPD_2in13_V3_HEIGHT, 30, BLACK, 2, DRAW_FILL_FULL);
     Paint_DrawString_EN(5, 5, title, &Font24, BLACK, WHITE);
 }
-
-
 
 typedef enum {
     STYLE_DEFAULT,
